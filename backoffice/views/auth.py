9876116de4ab04448829672629e4d67e7c053a77
Chihaan <gavet.vadim@gmail.com>
2026-07-23 16:29:11 +0200

    feat: implémente la logique de la route login

--- backoffice/views/auth.py
+++ backoffice/views/auth.py
@@ -1,6 +1,13 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, flash,
+    redirect, url_for,
+    render_template)
+from flask_login import login_user
+from sqlalchemy import select
 
 from forms import LoginForm
+from db import SessionLocal
+from models import User
 from services.auth import check_password
 
 auth_bp = Blueprint("auth", __name__)
@@ -10,4 +17,26 @@ auth_bp = Blueprint("auth", __name__)
 def login():
     """Affiche le formulaire de login et traite la connexion."""
     form = LoginForm()
-    return "TODO"
+    if form.validate_on_submit():
+        with SessionLocal() as session:
+            user = session.execute(
+                select(User)
+                .where(User.username == form.username.data)
+            ).scalar_one_or_none()
+
+            invalid = (
+                user is None
+                or user.deleted_at is not None
+                or not user.is_active
+            )
+            
+            if invalid:
+                flash("Identifiant ou mot de passe incorrect.")
+                return redirect(url_for("auth.login"))
+
+            if not check_password(user, form.password.data):
+                flash("Identifiant ou mot de passe incorrect.")
+                return redirect(url_for("auth.login"))
+            login_user(user)
+            return redirect(url_for("dashboard"))
+    return render_template("login.html", form=form)
