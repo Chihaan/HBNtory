2791f6c73e153a2302a6a8c7dfbf10d95514c23b
Chihaan <gavet.vadim@gmail.com>
2026-07-25 12:47:40 +0200

    feat: ajoute le formulaire de création d'utilisateur

--- backoffice/forms.py
+++ backoffice/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SelectField
 from wtforms.validators import DataRequired
 
 
@@ -7,3 +7,12 @@ class LoginForm(FlaskForm):
     """Formulaire de connexion : identifiant et mot de passe."""
     username = StringField("Nom d'utilisateur", validators=[DataRequired()])
     password = PasswordField("Mot de passe", validators=[DataRequired()])
+
+
+class UserCreateForm(FlaskForm):
+    """Formulaire de création d'un common user."""
+    username = StringField("Nom d'utilisateur", validators=[DataRequired()])
+    password = PasswordField("Mot de passe", validators=[DataRequired()])
+    branch_id = SelectField(
+        "Succursale", coerce=int, validators=[DataRequired()]
+    )
